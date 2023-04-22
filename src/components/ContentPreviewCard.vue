817c7b530dbb39cec7b9fbad2c898538ae137b79
Chaz Schooler <me@chazschooler.io>
2023-04-22 18:24:19 -0400

    Blog Content Index
    Adds in the ContentIndex.astro layout
    Creates ContentPreviewGrid.vue and ContentPreviewCard.vue components for displaying simple previews of Astro content collections.
    Creates SiteMeta.vue component to conveniently and consistently apply multiple tags that belong to the `<head>` of a document.
    Extracts several tailwind styles to base.css
    Creates string.ts with a couple of utility type
    functions to start with.
    
    And a blog post in a pear tree.

---
+++ src/components/ContentPreviewCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{
+  contentType: string
+  description: string,
+  title: string,
+  slug: string
+}>();
+</script>
+
+<template>
+  <div class="border-0 box-border rounded-none drop-shadow-none sm:drop-shadow-sm sm:border-2 border-gray-300 sm:rounded sm:p-2 flex flex-col bg-white sm:hover:drop-shadow-lg">
+    <a :href="slug" class="w-100 block px-2">
+      <h3 class="text-lg">{{ title }}</h3>
+      <div class="h-0 my-2 w-100 border border-gray-300"></div>
+      <p>{{ description }}</p>
+    </a>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ContentPreviewGrid.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import ContentPreviewCard from "./ContentPreviewCard.vue";
+import type {CollectionEntry} from "astro:content";
+import {onMounted} from "vue";
+
+const props = withDefaults(defineProps<{
+  contentType: string,
+  contentlets: Array<CollectionEntry<any>>
+  titleKey?: string,
+  descriptionKey?: string
+}>(), {titleKey: "title", descriptionKey: "description"});
+
+onMounted(() => {
+})
+</script>
+
+<template>
+  <div class="grid gap-2">
+    <ContentPreviewCard v-for="c in contentlets" :title="c.data[titleKey]" :description="c.data[descriptionKey]" :slug="c.slug" :content-type="contentType"/>
+  </div>
+</template>
+
+<style scoped>
+div {
+  grid-template-columns: repeat(auto-fit, minmax(48ch, 1fr));
+}
+</style>
